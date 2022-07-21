faa54c11d2918e495695c32bec384de80238dd62
r-ushil <rushil.patel791@gmail.com>
2022-07-21 17:42:15 +0100

    feat: added stack to wrap elements on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { Container, Stack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Intro from '../components/intro'
 
 const Home: NextPage = () => {
   return (
-    <Intro />
+    <Stack
+        as="main"
+        spacing="144px"
+        justifyContent="center"
+        alignItems="flex-start"
+        px={{ base: '5vw', md: '10vw' }}
+        mt={{ base: '15vh', md: '22.5vh' }}
+      >
+        <Intro />
+      </Stack>
   )
 }
 
