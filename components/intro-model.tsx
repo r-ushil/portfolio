7f030170f1669eeb394057d14b3f04735a3eda9a
r-ushil <rushil.patel791@gmail.com>
2022-07-22 17:04:31 +0100

    feat: set initial camera for model

--- components/intro-model.tsx
+++ components/intro-model.tsx
@@ -8,8 +8,8 @@ import Mustang from './mustang'
 
 export default function IntroScene() {
     return (
-        <div style={{ width: "40vw", height: "50vh" }}>
-            <Canvas>
+        <div style={{ width: "35vw", height: "50vh" }}>
+            <Canvas camera={{ fov: 45, position: [3, 3, 5] }}>
                 <ambientLight intensity={0.1} />
                 <pointLight position={[0, 10, 0]} intensity={1} />
                 <pointLight position={[10, 0, 0]} intensity={1} />
