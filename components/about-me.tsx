92311a125205c1e3aa44ec98c4216d78bdacc317
r-ushil <rushil.patel791@gmail.com>
2022-07-25 11:22:38 +0100

    feat: added skeleton about me component with pictures of me in public

---
+++ components/about-me.tsx
@@ -0,0 +1,91 @@
+import {
+    Text,
+    Heading,
+    Image,
+    Flex,
+    Box,
+    chakra,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverBody,
+    PopoverArrow,
+    VStack,
+    HStack,
+  } from '@chakra-ui/react'
+
+  import styles from "../styles/cycle-text.module.css";
+
+
+  interface MoreInfoProps {
+    text: string,
+    content: React.ReactNode,
+  }
+  
+  export default function AboutMe() {
+
+    const MoreInfo = (props: MoreInfoProps) => {
+      return (
+        <div>
+          <Popover
+            trigger="hover"
+            placement={'right'}
+          >
+            <PopoverTrigger>
+              <chakra.span
+                color="button1"
+                cursor="help"
+              >
+                {props.text}
+              </chakra.span>
+            </PopoverTrigger>
+            <PopoverContent bg="secondary" borderColor="button1" color="white">
+              <PopoverArrow bg="textSecondary" />
+              <PopoverBody fontSize="sm" color="textPrimary">
+                {props.content}
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </div>
+      )
+    }
+  
+    return (
+      <>
+
+      <HStack spacing="200px" justifyContent="center" alignItems="flex-start">
+
+            
+
+            <VStack spacing={4}>
+                
+              <Heading color={"displayColor"}>
+                    About Me ⚡                
+              </Heading>
+              <Text
+                fontSize="display3" color="textSecondary"
+              >
+            
+                Hey! I'm Rushil Patel, and this is some text about{' '}
+                Other stuff! 🏀 blahblahblah aksdhalksfhsdkla ajsdfhaksjdfh askjfh aisdj ajshdaj jdhajdha jdhajdhj 
+              </Text>
+            </VStack>
+
+            <Flex alignItems="center" justifyContent="center" position="relative">
+              <div style={{ width: "30vw", height: "60vh" }}>
+              
+                <Image
+                  src="/me-3.png"
+                  w="100%"
+                  maxW={{ base: '400px', lg: '450px' }}
+                  maxH={{ base: '400px', lg: '450px' }}
+                  alt="Rushil Patel"
+                />
+                
+              </div>
+            </Flex>
+            
+        </HStack>
+      </>
+    )
+  }
