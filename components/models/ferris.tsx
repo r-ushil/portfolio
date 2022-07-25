eb502579ddd9974df98929d7a6b5f2b9102c0664
r-ushil <rushil.patel791@gmail.com>
2022-07-25 17:44:54 +0100

    feat: added ferris the crab model

---
+++ components/models/ferris.tsx
@@ -0,0 +1,54 @@
+import { Mesh, MeshStandardMaterial, Group } from "three";
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import { useFrame } from "@react-three/fiber";
+
+type GLTFResult = GLTF & {
+    nodes: {
+        ferris_mesh: Mesh;
+        ferris_mesh_1: Mesh;
+        ferris_mesh_2: Mesh;
+    };
+    materials: {
+        skin: MeshStandardMaterial;
+        eye_black: MeshStandardMaterial;
+        eye_white: MeshStandardMaterial;
+    };
+};
+
+export default function Ferris({ ...props }: JSX.IntrinsicElements["group"]) {
+    const group = useRef<Group>();
+    const { nodes, materials } = useGLTF("/models/ferris.glb") as GLTFResult;
+    useFrame(() => (group.current.rotation.y += 0.002));
+    return (
+        <group ref={group} {...props} dispose={null}>
+            <group
+                position={[0, 1.5, 0]}
+                rotation={[-Math.PI, 0, -Math.PI]}
+                scale={6}
+            >
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.ferris_mesh.geometry}
+                    material={materials.skin}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.ferris_mesh_1.geometry}
+                    material={materials.eye_black}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.ferris_mesh_2.geometry}
+                    material={materials.eye_white}
+                />
+            </group>
+        </group>
+    );
+}
+
+useGLTF.preload("/models/ferris.glb");
