74f00e095c1cf2d28fd6623142dc2091a1942d2a
r-ushil <rushil.patel791@gmail.com>
2022-07-25 17:45:28 +0100

    feat: updated about me section to use buttons to update models

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -1,84 +1,74 @@
 import {
-    Text,
-    Heading,
-    Image,
-    Flex,
-    chakra,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverBody,
-    PopoverArrow,
-    VStack,
-    HStack,
-  } from '@chakra-ui/react'
+  Text,
+  Heading,
+  Flex,
+  VStack,
+  HStack,
+  Button,
+} from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import AboutMeCanvas from './about-me-canvas'
+import Laptop from './models/laptop'
+import Ferris from './models/ferris'
 
 
-  interface MoreInfoProps {
-    text: string,
-    content: React.ReactNode,
-  }
-  
-  export default function AboutMe() {
 
-    const MoreInfo = (props: MoreInfoProps) => {
-      return (
-        <div>
-          <Popover
-            trigger="hover"
-            placement={'right'}
-          >
-            <PopoverTrigger>
-              <chakra.span
-                color="button1"
-                cursor="help"
-              >
-                {props.text}
-              </chakra.span>
-            </PopoverTrigger>
-            <PopoverContent bg="secondary" borderColor="button1" color="white">
-              <PopoverArrow bg="textSecondary" />
-              <PopoverBody fontSize="sm" color="textPrimary">
-                {props.content}
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
-        </div>
-      )
-    }
-  
-    return (
-      <>
+interface MoreInfoProps {
+  text: string,
+  model_src?: string,
+}
+
+export default function AboutMe() {
+
+  // default display item - set as picture of me
+
+  const [modelSrc, setModelSrc] = useState<React.ReactNode>(null)
+  const [modelCanvas, setModelCanvas] = useState<React.ReactNode>()
+
+  useEffect(() => {
+
+    setModelCanvas(AboutMeCanvas({ src: modelSrc }))
+
+  }, [modelSrc])
+
+  return (
+    <>
       <VStack spacing={4} justifyContent="left" alignItems="flex-start">
         <Heading color={"displayColor"}>
-          About Me ⚡                
+          About Me ⚡
         </Heading>
         <HStack spacing="200px" justifyContent="center" alignItems="flex-start">
-          
+
           <VStack spacing={4} justifyContent="left" alignItems="flex-start">
             <Text
               fontSize="display3" color="textSecondary">
-              
-                  Hey! I'm Rushil Patel, and this is some text about{' '}
-                  Other stuff! 🏀 blahblahblah aksdhalksfhsdkla ajsdfhaksjdfh askjfh aisdj ajshdaj jdhajdha jdhajdhj 
-            
+
+              Hey! I'm {' '}
+              <Button fontSize="display3" color="#3CCF91" variant='link' onClick={() => { setModelSrc(null) }}>
+                Rushil Patel
+              </Button>
+
+              , and this is some text about{' '}
+              Other stuff! 🏀  askjfh aisdj ajshdaj jdhajdha jdhajdhj{' '}
+              <Button fontSize="display3" color="#3CCF91" variant='link' onClick={() => { setModelSrc(<Laptop />) }}>
+                click
+              </Button>
+
+              {' '}blahblahblah aksdhalksfhsdkla ajsdfhaksjdfh, and also, my favourite programming language is {' '}
+              <Button fontSize="display3" color="#3CCF91" variant='link' onClick={() => { setModelSrc(<Ferris />) }}>
+                Rust
+              </Button>
+
+
             </Text>
           </VStack>
 
           <Flex alignItems="center" justifyContent="center" position="relative">
-            <div style={{ width: "30vw", height: "60vh" }}>
-              <Image
-                  src="/me-3.png"
-                  w="100%"
-                  maxW={{ base: '400px', lg: '450px' }}
-                  maxH={{ base: '400px', lg: '450px' }}
-                  alt="Rushil Patel"
-                />        
-            </div>
+            {modelCanvas}
           </Flex>
-              
-          </HStack>
-        </VStack>
-      </>
-    )
-  }
+
+        </HStack>
+      </VStack>
+    </>
+  )
+}
