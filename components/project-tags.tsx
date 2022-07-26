b5e161131cce72ca8c2bfa31c6fa839b6d526438
r-ushil <rushil.patel791@gmail.com>
2022-07-26 18:03:54 +0100

    feat: created project-tag function to get tag info for projects

---
+++ components/project-tags.tsx
@@ -0,0 +1,58 @@
+import { Tag, TagLabel, TagLeftIcon } from "@chakra-ui/react"
+import { IconType } from "react-icons"
+
+import {
+    FaReact,
+    FaRust,
+    FaNode,
+  } from 'react-icons/fa'
+
+import {
+  SiTypescript,
+  SiNextdotjs
+} from 'react-icons/si'
+
+export enum ProjectTags {
+    REACT,
+    NODE,
+    TYPESCRIPT,
+    RUST,
+    NEXT,
+}
+
+interface getTagsProps {
+    tags: ProjectTags[]
+}
+
+interface tagInfo {
+    color: string,
+    icon: IconType
+}
+
+function getTagInfo(tag: ProjectTags) {
+    switch (tag) {
+        case ProjectTags.REACT: return {color: 'blue', icon: FaReact}
+        case ProjectTags.NODE: return {color: 'green', icon: FaNode}
+        case ProjectTags.TYPESCRIPT: return {color: 'purple', icon: SiTypescript}
+        case ProjectTags.RUST: return {color: 'orange', icon: FaRust}
+        case ProjectTags.NEXT: return {color: 'white', icon: SiNextdotjs}
+    }
+}
+
+export default function getTags({tags}: getTagsProps) {
+
+    const tagsAsJsx = tags.map((item) => {
+      const tagInfo = getTagInfo(item);
+      return (
+        <Tag
+          key={item}
+          colorScheme={tagInfo.color}
+          size='md'
+        >
+          <TagLeftIcon as={tagInfo.icon}></TagLeftIcon>
+          <TagLabel>{item}</TagLabel>
+        </Tag>
+    )})
+
+    return tagsAsJsx;
+}
