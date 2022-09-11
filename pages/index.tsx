3740be65f35f2af8dba5f951ea956da54e75f8db
r-ushil <rushil.patel@cub3.com>
2022-09-11 12:06:46 +0100

    feat: centralised intro model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Wrap, Stack } from '@chakra-ui/react'
+import { Wrap, Stack, WrapItem } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import AboutMe from '../components/sections/about-me'
 import Container from '../components/sections/container'
@@ -8,25 +8,32 @@ import Projects from '../components/sections/projects'
 
 const Home: NextPage = () => {
   return (
-        <Container>
-          <Stack
-              as="main"
-              spacing="144px"
-              justifyContent="center"
-              alignItems="flex-start"
-              px={{ base: '5vw', md: '7.5vw' }}
-              mt={{ base: '17.5vh', md: '22.5vh' }}
-            >
-            <Wrap spacing="100px" justifyContent="center" alignItems="flex-start">
-              <Intro />
-              <IntroScene />
-            </Wrap>
-            
-            <AboutMe />
-            
-            <Projects />
-            </Stack>
-      </Container>
+    <Container>
+      <Stack
+        as="main"
+        spacing="144px"
+        justifyContent="center"
+        alignItems="flex-start"
+        px={{ base: '5vw', md: '7.5vw' }}
+        mt={{ base: '17.5vh', md: '22.5vh' }}
+      >
+        <Wrap spacing="100px" justify="center" align="center">
+
+          <WrapItem>
+            <Intro />
+          </WrapItem>
+
+          <WrapItem>
+            <IntroScene />
+          </WrapItem>
+
+        </Wrap>
+
+        <AboutMe />
+
+        <Projects />
+      </Stack>
+    </Container>
   )
 }
 
